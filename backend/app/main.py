dd401957e3a56d7bbf3b8d25fc47e304fa6f5acb
RiteshKumar2e <riteshkumar90359@gmail.com>
2025-12-30 20:34:09 +0530

    updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,6 +33,7 @@ app.add_middleware(
         "https://riteshkr.online",
         "http://localhost:5173",
         "http://localhost:5174",
+        "http://riteshkr.online"
     ],
     allow_credentials=True,
     allow_methods=["*"],
